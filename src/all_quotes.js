1c30392f357f2d16258ffd21ef162280108ed038
NSD <73041332+Nisedo@users.noreply.github.com>
2021-08-05 09:05:29 +0200

    modifs pour codepen

--- src/all_quotes.js
+++ src/all_quotes.js
@@ -3,7 +3,7 @@ fetch("https://api.kanye.rest")
     .then(res => res.json())
     .then(quote => {
         quotesDiv.innerHTML += `<br>`
-        quotesDiv.innerHTML += `<p style="font-size: 25px"><em>- "${quote.quote}"</em></p>`
+        quotesDiv.innerHTML += `<p style="font-size: 25px"><em>"${quote.quote}"</em></p>`
         quotesDiv.innerHTML += `<p style="text-decoration: underline">Kanye West</p>`
     })
 
@@ -12,7 +12,7 @@ fetch("https://api.booba.cloud")
     .then(res => res.json())
     .then(quote => {
         quotesDiv2.innerHTML += `<br>`
-        quotesDiv2.innerHTML += `<p style="font-size: 25px"><em>- "${quote.quote}"</em></p>`
+        quotesDiv2.innerHTML += `<p style="font-size: 25px"><em>"${quote.quote}"</em></p>`
         quotesDiv2.innerHTML += `<p style="text-decoration: underline">Booba</p>`
     })
 
@@ -21,6 +21,6 @@ fetch("https://api.chucknorris.io/jokes/random")
     .then(res => res.json())
     .then(value => {
         quotesDiv3.innerHTML += `<br>`
-        quotesDiv3.innerHTML += `<p style="font-size: 25px"><em>- "${value.value}"</em></p>`
+        quotesDiv3.innerHTML += `<p style="font-size: 25px"><em>"${value.value}"</em></p>`
         quotesDiv3.innerHTML += `<p style="text-decoration: underline">Chuck Norris</p>`
     })
